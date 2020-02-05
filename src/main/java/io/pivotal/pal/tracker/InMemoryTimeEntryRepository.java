001b01d42bcc751a10bc819ee3da2f30c26e50be
Bill Kable <bkable@gmail.com>
2020-02-05 00:41:11 -0600

    jdbc

--- src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
+//@Repository
 public class InMemoryTimeEntryRepository implements TimeEntryRepository {
 
    public static Long id = 0l;
